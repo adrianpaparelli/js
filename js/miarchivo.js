035015b4fdd33cc28fcc2de9dc1af9df912a9e60
Adrian Paparelli <adrian@paparelli.com.ar>
2022-07-27 23:03:10 -0300

    Primer Desafio Complementario JS

---
+++ js/miarchivo.js
@@ -0,0 +1,11 @@
+let clave = prompt("Ingrese su clave")
+
+if (clave = 1978){
+    alert("Bienvenido al Sitio")
+}else if (clave != 1978){
+    alert("Acceso Restringido. Intente nuevamente")
+}else if (clave < 1978){
+    alert("Acceso Restringido. Intente nuevamente")
+}else if (clave > 1978){
+    alert("Acceso Restringido. Intente nuevamente")
+}
